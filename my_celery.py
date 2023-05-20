9fe2c62a5b99ccff195aa6a1877547c57cc57732
Cristib83 <cristibogdan08@gmail.com>
2023-05-20 11:05:37 +0300

    error logs handle

--- my_celery.py
+++ my_celery.py
@@ -1,7 +1,11 @@
 import os
+import logging
 from celery import Celery
 
 # Get Redis URL from environment variable, or use default URL
 redis_url = os.environ.get('REDIS_URL')
 # Create a Celery instance named 'tasks' with Redis as the broker
 celery_app = Celery('tasks', broker=redis_url)
+
+# Configure logging
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
--- tasks.py
+++ tasks.py
@@ -3,48 +3,58 @@ import os
 import requests
 from tempfile import NamedTemporaryFile
 from my_celery import celery_app
+import logging
 
 blockfrost_api_key = os.environ.get("BLOCKFROST_API_KEY")
 base_api = "https://cardano-mainnet.blockfrost.io/api/v0"
 
 @celery_app.task
 def generate_csv(policy_id):
-    all_assets = []
-    page = 1
+    try:
+        all_assets = []
+        page = 1
 
-    while True:
-        response = requests.get(f'{base_api}/assets/policy/{policy_id}?page={page}', headers={"project_id": blockfrost_api_key})
-        if len(response.json()) == 0:
-            break
+        while True:
+            response = requests.get(f'{base_api}/assets/policy/{policy_id}?page={page}', headers={"project_id": blockfrost_api_key})
+            response.raise_for_status()  # Raise an exception for non-2xx response codes
 
-        for asset in response.json():
-            temp = {}
-            asset_hex_name = asset["asset"][int(len(str(policy_id))):]
-            if all(c in "0123456789abcdefABCDEF" for c in asset_hex_name):
-                temp['name'] = bytearray.fromhex(asset_hex_name).decode()
-            else:
-                temp['name'] = 'INVALID HEX STRING'
-            temp['asset'] = asset['asset']
-            all_assets.append(temp)
-        page += 1
+            if len(response.json()) == 0:
+                break
 
-    address_assets = {}
-    for asset in all_assets:
-        response = requests.get(f'{base_api}/assets/{asset["asset"]}/addresses', headers={"project_id": blockfrost_api_key})
-        for item in response.json():
-            address = item['address']
-            if address not in address_assets:
-                address_assets[address] = 1
-            else:
-                address_assets[address] += 1
+            for asset in response.json():
+                temp = {}
+                asset_hex_name = asset["asset"][int(len(str(policy_id))):]
+                if all(c in "0123456789abcdefABCDEF" for c in asset_hex_name):
+                    temp['name'] = bytearray.fromhex(asset_hex_name).decode()
+                else:
+                    temp['name'] = 'INVALID HEX STRING'
+                temp['asset'] = asset['asset']
+                all_assets.append(temp)
+            page += 1
 
-    data = []
-    for address, num_assets in address_assets.items():
-        data.append([address, num_assets])
+        address_assets = {}
+        for asset in all_assets:
+            response = requests.get(f'{base_api}/assets/{asset["asset"]}/addresses', headers={"project_id": blockfrost_api_key})
+            response.raise_for_status()  # Raise an exception for non-2xx response codes
 
-    with NamedTemporaryFile(mode='w', delete=False, newline='') as temp_file:
-        csv_writer = csv.writer(temp_file)
-        csv_writer.writerow(['Address', 'Number of Assets'])
-        csv_writer.writerows(data)
+            for item in response.json():
+                address = item['address']
+                if address not in address_assets:
+                    address_assets[address] = 1
+                else:
+                    address_assets[address] += 1
+
+        data = []
+        for address, num_assets in address_assets.items():
+            data.append([address, num_assets])
+
+        with NamedTemporaryFile(mode='w', delete=False, newline='') as temp_file:
+            csv_writer = csv.writer(temp_file)
+            csv_writer.writerow(['Address', 'Number of Assets'])
+            csv_writer.writerows(data)
 
         return temp_file.name
+
+    except Exception as e:
+        logging.error(f'Error generating CSV: {str(e)}')
+        raise
