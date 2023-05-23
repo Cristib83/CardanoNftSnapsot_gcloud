6bb15e3350428363890a188100f86bccb303f970
Cristib83 <cristibogdan08@gmail.com>
2023-05-23 08:22:48 +0300

    36

--- my_celery.py
+++ my_celery.py
@@ -1,19 +1,13 @@
 import os
 import logging
 from celery import Celery
-from urllib.parse import urlparse
 import redis
 
 
-url = urlparse(os.environ.get('REDIS_URL'))
-broker_url = f"redis://{url.hostname}:{url.port}/{url.path[1:]}"
-result_backend = f"redis://{url.hostname}:{url.port}/{url.path[1:]}"
-
-
-# Get Redis URL from environment variable, or use default URL
-redis_url =os.environ.get('REDIS_URL')
+redis_url = os.environ.get('REDIS_URL')
 r = redis.from_url(redis_url)
-# Create a Celery instance named 'tasks' with Redis as the broker
+
+# Create a Celery instance named 'tasks' with Redis as the broker and result backend
 celery_app = Celery('tasks', broker=redis_url, backend=redis_url)
 
 # Configure logging
